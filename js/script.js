380a60ba5bf8829834fcc966fb86bf3047745fd3
SJPadikkoth <sjpadikkoth@gmail.com>
2018-03-07 15:33:25 +1100

    Initial Version

---
+++ js/script.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="angular.min.js" />
+var app = angular.module("myApp", []);
+app.controller("myController", function ($scope) {
+    var items = [
+        {
+            Name: "Item1",
+            Price: "$40"
+        },
+        {
+            Name: "Item2",
+            Price: "$10"
+        },
+        {
+            Name: "Item3",
+            Price: "$30"
+        },
+        {
+            Name: "Item4",
+            Price: "$50"
+        },
+        {
+            Name: "Item5",
+            Price: "$20"
+        }
+    ]
+
+    $scope.items = items;
+});
